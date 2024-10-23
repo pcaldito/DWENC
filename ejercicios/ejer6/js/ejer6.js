c34e5700192158f01fde41931545054a13880535
pcaldito <147615008+pcaldito@users.noreply.github.com>
2024-10-23 10:38:42 +0200

    Delete ejercicios directory

--- ejercicios/ejer6/js/ejer6.js
+++
@@ -1,8 +0,0 @@
-let num = prompt("Introduce un número para mostrar su tabla de multiplicar:");
-num = parseInt(num);  
-let i = 1;
-
-while(i <= 10) {
-    console.log(num + " x " + i + " = " + (num * i));
-    i++;
-}
